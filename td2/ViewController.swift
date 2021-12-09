1e92f576a745b7a79c5a3b8d55dcedc5ca8a8834
ghariq <bapt.godard15@gmail.com>
2021-12-09 08:30:39 +0100

    second commit

---
+++ td2/ViewController.swift
@@ -0,0 +1,85 @@
+//
+//  ViewController.swift
+//  td2
+//
+//  Created by Baptiste Godard on 30/11/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    @IBOutlet weak var lesPhotosDuFun: UITableView!
+    
+    var myData = [MyData]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        for i in 1...50 {
+            let num = i%3+1
+            if (num==1){
+                myData.append(MyData("Isaac","Jeu de fou furieux", String(num)))
+            }
+            else if (num==2){
+                myData.append(MyData("Lama","Le plus beau des animaux !!", String(num)))
+            }
+            else {
+                myData.append(MyData("BG","Valentin, motard de France et de Navarre", String(num)))
+            }
+        }
+        
+        lesPhotosDuFun.dataSource = self
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return myData.count;
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "lacelluledejoline", for: indexPath) as! TableViewCell
+        
+        cell.ltitrelo.text = myData[indexPath.row]._title
+        cell.ldescriptionlo.text = myData[indexPath.row]._desc
+        cell.limagelo.image = UIImage(named: myData[indexPath.row]._nom_toph)
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Find the row of the cell
+            let row = indexPath.row
+            myData.remove(at: row)
+            lesPhotosDuFun.deleteRows(at: [indexPath], with: .fade)
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "details" {
+            let detailsViewController = segue.destination as! DetailsViewController
+            let myIndexPath = lesPhotosDuFun.indexPathForSelectedRow!
+            let row = myIndexPath.row
+            detailsViewController.myData = myData[row]
+        }
+    }
+    
+    
+    @IBAction func unwindToMainView(_ unwindSegue: UIStoryboardSegue) {
+        let addViewController = unwindSegue.source as! AddViewController
+        if unwindSegue.identifier == "cancel" {
+            addViewController.dismiss(animated: true, completion: nil)
+        }
+        if unwindSegue.identifier == "save" {
+            if let myTitle = addViewController.myTitle.text, let myDescription = addViewController.myDescription.text {
+                let new_data = MyData(title: myTitle,
+                                      description: myDescription,
+                                      image: String(Int.random(in: 1...5)))
+                myData.append(new_data)
+                lesPhotosDuFun.reloadData()
+            }
+        }
+        // Use data from the view controller which initiated the unwind segue
+    }
+}
