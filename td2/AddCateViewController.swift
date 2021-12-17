a1317dd47c62e69d8c44d9cf3153108b519f5e2f
BerAtozz <valentinmania@gmail.com>
2021-12-17 16:09:12 +0100

    correction add cate storyboard

---
+++ td2/AddCateViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AddCateViewController.swift
+//  td2
+//
+//  Created by Valentin Berthoz on 17/12/2021.
+//
+
+import UIKit
+
+class AddCateViewController: UIViewController {
+
+    @IBOutlet weak var nom_cate: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
